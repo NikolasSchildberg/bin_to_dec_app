23568c4fde68af38ce428e0137d5b798e6dd1b98
Nikolas Schildberg <nikolas.schildberg@gmail.com>
2022-05-28 21:10:29 -0300

    Control input fields (make them controlled components)

--- src/ConversionForm/NumEntry.tsx
+++ src/ConversionForm/NumEntry.tsx
@@ -1,13 +1,18 @@
 interface NumEntryProps{
     displayThis: string;
     fieldName: string;
+    onChange: (event: any) => void;
 }
 
-export function NumEntry({displayThis, fieldName}: NumEntryProps){
+export function NumEntry({displayThis, fieldName, onChange}: NumEntryProps){
     return (
         <div className="m-10 flex flex-col items-center">
             <h1 className="font-bold text-2xl">{fieldName}</h1>
-            <input className="m-4 w-auto h-10 text-center text-gray-700" placeholder={displayThis}></input>
+            <input
+                className="m-4 w-auto h-10 text-center text-gray-700"
+                value={displayThis}
+                onChange={(event)=> onChange(event)}
+            ></input>
         </div>
     )
 }
--- src/ConversionForm/index.tsx
+++ src/ConversionForm/index.tsx
@@ -1,9 +1,21 @@
 import { ArrowsHorizontal } from "phosphor-react";
+import { ChangeEvent, useState } from "react";
 import { NumEntry } from "./NumEntry";
 import { SubmitButton } from "./SubmitButton";
 
 export function ConversionForm(){
-    
+    const [binField, setBinField] = useState('Ex: 101010');
+    const [decField, setDecField] = useState('Ex: 42');
+
+    function handleChangeBinField(event: ChangeEvent<HTMLInputElement>){
+        setBinField(event.target.value);
+    }
+
+    function handleChangeDecField(event: ChangeEvent<HTMLInputElement>){
+        setDecField(event.target.value);
+    }
+
+
     return (
         <div>
             <form 
@@ -11,9 +23,17 @@ export function ConversionForm(){
                 action=""
             >
                 <div className="flex items-center">
-                    <NumEntry displayThis='101010' fieldName="Binary"/>
+                    <NumEntry
+                        fieldName="Binary"
+                        displayThis={binField}
+                        onChange={(event)=> handleChangeBinField(event)}
+                    />
                     <ArrowsHorizontal size={32} className='mt-5'/>
-                    <NumEntry displayThis='42' fieldName="Decimal"/>
+                    <NumEntry
+                        fieldName="Decimal"
+                        displayThis={decField}
+                        onChange={(event) => handleChangeDecField(event)}
+                    />
                 </div>
                 <SubmitButton />
             </form>
